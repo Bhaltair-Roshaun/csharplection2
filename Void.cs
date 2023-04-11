22155d65841a7bcb804cb63e35f54bcf1c3b9cd7
Bhaltair <gilliarool@mail.ru>
2023-04-11 22:13:21 +0300

    04.Void!

--- Void.cs
+++ Void.cs
@@ -1,16 +1,28 @@
-﻿int[] array = { 1, 12, 31, 4, 15,18, 16, 17, 18, };
-
-int n = array.Length;
-int find = 18;
-
-int index = 0;
-
-while (index < n)
+﻿
+void FillArray(int[] collection)
 {
-    if (array[index] == find)
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
     {
-        Console.WriteLine(index);
-        break;
-    }
+        collection[index] = new Random().Next(1,10);
         index++;
+    }
 }
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);  
