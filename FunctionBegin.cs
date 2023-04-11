b3c11990cf939d6fc1b2ec332c4876d2fe355839
Bhaltair <gilliarool@mail.ru>
2023-04-11 21:26:10 +0300

    02.Array

--- FunctionBegin.cs
+++ FunctionBegin.cs
@@ -5,25 +5,13 @@
     if (arg3 > result) result = arg3;
     return result;
 }
+int[] array = { 11, 28, 3, 46, 55, 6, 77, 8, 9, };
+array[0]= 12;
 
-int a1 = 23;
-int b1 = 24;
-int c1 = 54;
-int a2 = 223;
-int b2 = 214;
-int c2 = 544;
-int a3 = 2523;
-int b3 = 52234;
-int c3 = 5144;
-
-
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-// int max = Max(max1, max2, max3);
 
 int max = Max(
-    Max(a1,b1,c1), 
-    Max(a2,b2,c2), 
-    Max(a3,b3,c3));
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
 Console.WriteLine(max);
